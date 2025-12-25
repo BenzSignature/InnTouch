6be37bd0d273c76ab8e26310f797df0955e41f08
Thanakrit <phattharaweepiyathonglai@MacBook-Air-khxngPhattharawee.local>
2025-12-25 15:32:30 +0700

    refactor(icons) : update language icon

--- lib/src/config/app_icons.dart
+++ lib/src/config/app_icons.dart
@@ -2,7 +2,7 @@ import 'package:inn_touch/src/config/export_config.dart';
 
 class AppIcons {
   static const profile = FontAwesomeIcons.solidUser;
-  static const language = Icons.language;
+  static const language = FontAwesomeIcons.globe;
   static const payment = FontAwesomeIcons.solidCreditCard;
   static const security = FontAwesomeIcons.lock;
   static const notifications = FontAwesomeIcons.solidBell;
