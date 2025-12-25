36b7272592396e23ad27131692e2ab39acc6a97f
Thanakrit <phattharaweepiyathonglai@MacBook-Air-khxngPhattharawee.local>
2025-12-25 11:30:19 +0700

    (feat) : add official icons
    - add file app_icons
    - add official icons in app_icons.dart

---
+++ lib/src/config/app_icons.dart
@@ -0,0 +1,11 @@
+import 'package:inn_touch/src/config/export_config.dart';
+
+class AppIcons {
+  static const profile = FontAwesomeIcons.solidUser;
+  static const language = FontAwesomeIcons.language;
+  static const payment = FontAwesomeIcons.creditCard;
+  static const security = FontAwesomeIcons.shieldHalved;
+  static const notifications = FontAwesomeIcons.bell;
+  static const support = FontAwesomeIcons.headset;
+  static const arrowRight = FontAwesomeIcons.chevronRight;
+}
