83a5f7668b4dc330d315a3ac4536b5d332559e74
Thanakrit <phattharaweepiyathonglai@MacBook-Air-khxngPhattharawee.local>
2025-12-25 11:59:39 +0700

    feat(config) : implement official Icons in app_icons.dart
    - add more Icons in app_icons.dart

--- lib/src/config/app_icons.dart
+++ lib/src/config/app_icons.dart
@@ -2,10 +2,11 @@ import 'package:inn_touch/src/config/export_config.dart';
 
 class AppIcons {
   static const profile = FontAwesomeIcons.solidUser;
-  static const language = FontAwesomeIcons.language;
-  static const payment = FontAwesomeIcons.creditCard;
-  static const security = FontAwesomeIcons.shieldHalved;
-  static const notifications = FontAwesomeIcons.bell;
+  static const language = Icons.language;
+  static const payment = FontAwesomeIcons.solidCreditCard;
+  static const security = FontAwesomeIcons.lock;
+  static const notifications = FontAwesomeIcons.solidBell;
   static const support = FontAwesomeIcons.headset;
-  static const arrowRight = FontAwesomeIcons.chevronRight;
+  static const arrowRight = FontAwesomeIcons.angleRight;
+  static const logOut = FontAwesomeIcons.rightFromBracket;
 }
